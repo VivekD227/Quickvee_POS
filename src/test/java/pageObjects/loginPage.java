66810a47ac6547d1e04cfed15e5c2b6781956ac7
Vivek Dubey <vivek.dubey521@gmail.com>
2025-08-18 09:45:24 -0700

    extends waitHelper class in LoginPage

--- src/test/java/pageObjects/loginPage.java
+++ src/test/java/pageObjects/loginPage.java
@@ -3,22 +3,18 @@ package pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-
-import io.appium.java_client.android.AndroidDriver;
-import utilities.BaseClass;
 import utilities.waitHelper;
 
-public class loginPage extends BaseClass {
+public class loginPage extends waitHelper {
 
 	public AndroidDriver driver;
-	waitHelper wait;
 	public loginPage(AndroidDriver driver) {
-
+		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-		wait = new waitHelper(driver);
 	}
 		
 	@AndroidFindBy(xpath = "//android.widget.ImageView[@content-desc='Quickvee POS (Dev)']")
@@ -76,92 +72,92 @@ public class loginPage extends BaseClass {
 	private WebElement errorPasswordInput;
 
 	public boolean quickveeLogoDisplay() {
-		wait.visiblityOfElement(quickveeLogo);
+		visiblityOfElement(quickveeLogo);
 		return quickveeLogo.isDisplayed();
 	}
 	
 	public boolean loginTextDisplay() {
-		wait.visiblityOfElement(loginText);
+		visiblityOfElement(loginText);
 		return loginText.isDisplayed();
 	}
 	
 	public boolean storeTextDisplay() {
-		wait.visiblityOfElement(storeNameText);
+		visiblityOfElement(storeNameText);
 		return storeNameText.isDisplayed();
 	}
 	
 	public boolean emailTextDisplay() {
-		wait.visiblityOfElement(emailText);
+		visiblityOfElement(emailText);
 		return emailText.isDisplayed();
 	}
 	
 	public boolean passwordText() {
-		wait.visiblityOfElement(passwordText);
+		visiblityOfElement(passwordText);
 		return passwordText.isDisplayed();
 	}
 	
 	public void storeFieldClicked() {
-		wait.elementClick(storeField);
+		elementClick(storeField);
 		storeField.click();
 	}
 	
 	public void emailFieldClicked() {
-		wait.elementClick(emailField);
+		elementClick(emailField);
 		emailField.click();
 	}
 	
 	public void passwordField() {
-		wait.elementClick(passwordField);
+		elementClick(passwordField);
 		passwordField.click();
 	}
 	
 	public void storeToolTipClicked() {
-		wait.elementClick(storeToolTip);
+		elementClick(storeToolTip);
 		storeToolTip.click();
 	}
 	
 	public void showPasswordBtnClicked() {
-		wait.elementClick(showPasswordBtn);
+		elementClick(showPasswordBtn);
 		showPasswordBtn.click();
 	}
 	
 	public void forgotPassBtnClicked() {
-		wait.elementClick(forgotPassBtn);
+		elementClick(forgotPassBtn);
 		forgotPassBtn.click();
 	}
 	
 	public boolean manadatoryTextDisplay() {
-		wait.visiblityOfElement(manadatoryText);
+		visiblityOfElement(manadatoryText);
 		return manadatoryText.isDisplayed();
 	}
 	
 	public void logiBtnClicked() {
-		wait.elementClick(loginBtn);
+		elementClick(loginBtn);
 		loginBtn.click();
 	}
 	
 	public boolean invalidErrorMsgDisplay() {
-		wait.visiblityOfElement(invalidErrorMsg);
+		visiblityOfElement(invalidErrorMsg);
 		return invalidErrorMsg.isDisplayed();
 	}
 	
 	public void invalidOKBtnClicked() {
-		wait.elementClick(invalidOKBtn);
+		elementClick(invalidOKBtn);
 		invalidOKBtn.click();
 	}
 	
 	public boolean errorStoreInputDisplay() {
-		wait.visiblityOfElement(errorStoreInput);
+		visiblityOfElement(errorStoreInput);
 		return errorStoreInput.isDisplayed();
 	}
 	
 	public boolean errorEmailInputDisplay() {
-		wait.visiblityOfElement(errorEmailInput);
+		visiblityOfElement(errorEmailInput);
 		return errorEmailInput.isDisplayed();
 	}
 	
 	public boolean errorPasswordInputDisplay() {
-		wait.visiblityOfElement(errorPasswordInput);
+		visiblityOfElement(errorPasswordInput);
 		return errorPasswordInput.isDisplayed();
 	}
 	
